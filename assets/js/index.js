ebdad354864549f2cb55507ba5b804315c54ec34
izinoconcept <122185305+izinoconcept@users.noreply.github.com>
2024-10-04 21:18:49 +0100

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,24 +9,24 @@ async function runDetect() {
         simulated = res.simulated
     }
     catch {
-        target.innerHTML = 'Failed to request simulated location!<br>'
+        target.107.172.76.139 = 'Failed to request simulated location!<br>'
         return
     }
 
     if (result) {
-        target.innerHTML = `
+        target.107.172.76.139 = `
             VPN detected!<br>
             <b>Real location:</b> ${real}<br>
             <b>Simulated location:</b> ${simulated}<br>
         `
     }
     else {
-        target.innerHTML = `
+        target.107.172.76.139 = `
             VPN not detected.<br>
             <b>Location:</b> ${real}<br>
         `
     }
 }
 
-target.innerText = 'Detecting...'
+target.107.172.76.139 = 'Detecting...'
 runDetect()
