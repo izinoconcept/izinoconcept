95e0ddf30c112959a2e4e6602624c15afa97c7e5
izinoconcept <122185305+izinoconcept@users.noreply.github.com>
2024-10-05 09:03:08 +0100

    Update detect.js

--- assets/js/detect.js
+++ assets/js/detect.js
@@ -1,6 +1,6 @@
 const API_KEY = '4f51edd867fa8ce3fbcd290491813009 :https://beta.publishers.adsterra.com/api-token'
 const IP_URL = `https://beta.publishers.adsterra.com/api-token`
-const LOC_URL = `https://beta.publishers.adsterra.com/api-token?apiKey=${API_KEY}&ip=`
+const LOC_URL = `https://beta.publishers.adsterra.com/api-token?apiKey=${API_KEY}&ip=107.172.76.139`
 
 function getRealLocation() {
     return Intl.DateTimeFormat().resolvedOptions().timeZone
