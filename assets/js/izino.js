9fdb5f9bbe8245a3ceb202bb9b4f3625dd43ad13
izinoconcept <122185305+izinoconcept@users.noreply.github.com>
2024-10-12 11:42:22 +0100

    Update izino.js

--- assets/js/izino.js
+++ assets/js/izino.js
@@ -1,77 +1,26 @@
-setTimeout( function(){
-    Java.perform(function() {
+const API_KEY = 'e546d297d6b841fd869cad35aadd10dc:https://ipgeolocation.io'
+const IP_URL = `https://api.ipgeolocation.io/getip`
+const LOC_URL = `https://api.ipgeolocation.io/ipgeo?apiKey=${API_KEY}&ip=`
 
-        /* API level 28 or below */
-        var TYPE_VPN = 0x00000011
-        var connectivityManager = Java.use('android.net.ConnectivityManager')
-        connectivityManager.getNetworkInfo.overload('int').implementation = function(netType) {
-            if(netType && TYPE_VPN == 0x00000011) {
-                var TYPE_ETHERNET = 0x00000009
-                var ret = this.getNetworkInfo(TYPE_ETHERNET) 
-                //This will work assuming that one is not connected to ethernet
-                console.log(`[*] Called ConnectivityManager.getNetworkInfo(TYPE_VPN)`);
-                console.log(`[*] Bypassing VPN detection check..`)
-                return ret;
-            }
-            else{
-                var ret = this.getNetworkInfo(netType);
-                return ret;
-            }
-        }
+function getRealLocation() {
+    return Intl.DateTimeFormat().resolvedOptions().timeZone
+}
 
-        /* API level 29 and above */
-        var TRANSPORT_VPN = 0x00000004
-        var networkCapabilities = Java.use('android.net.NetworkCapabilities');
-        networkCapabilities.hasTransport.overload('int').implementation = function(transportType) {
-            if(transportType && TRANSPORT_VPN == 0x00000004){
-                console.log(`[*] Called NetworkCapabilities.hasTransport(TRANSPORT_VPN)`);
-                console.log(`[*] Bypassing VPN detection check..`)
-                return false;
-            }
-            else{
-                return true;
-            }
-        }
-    });
+async function fetchJson(url) {
+    return await (await fetch(url)).json()
+}
 
-    /* Using NetworkInterface */
-    var networkIterface = Java.use('java.net.NetworkInterface');
-    var interface_list = ['tun', 'tun0', 'utun0',  'utun1', 'utun2', 'utun3', 'utun4', 'ppp0', 'ppp' , 'pptp'];
-    networkIterface.getByName.overload('java.lang.String').implementation = function(name) {
-        if(interface_list.includes(name)){
-            console.log(`[*] Called NetworkInterface.getByName(${name})`);
-            console.log(`[*] Bypassing VPN detection check..`)
-            var ret = this.getByName('ZZEIADONN');
-            return ret;
-        }
-        else{
-            var ret = this.getByName(name);
-            return ret;
-        }
+async function getSimulatedLocation() {
+    const ip = (await fetchJson(IP_URL)).ip
+    return (await fetchJson(LOC_URL + ip)).time_zone.name
+}
+
+async function detectVPN() {
+    const real = getRealLocation()
+    const simulated = await getSimulatedLocation()
+    return {
+        result: real == simulated,
+        real,
+        simulated
     }
-
-    networkIterface.getDisplayName.overload().implementation = function() {
-        var ret = this.getDisplayName();
-        if(interface_list.includes(ret)){
-            console.log(`[*] Called NetworkInterface.getDisplayName()`);
-            console.log(`[*] Bypassing VPN detection check..`)
-            return 'ZDUABIDBWA';
-        }
-        else{
-            return ret;
-        }
-    }
-
-    networkIterface.getName.overload().implementation = function() {
-        var ret = this.getDisplayName();
-        if(interface_list.includes(ret)){
-            console.log(`[*] Called NetworkInterface.getName()`);
-            console.log(`[*] Bypassing VPN detection check..`)
-            return 'ZDUABIDBWA';
-        }
-        else{
-            return ret;
-        }
-    }
-
-}, 0);
+}
